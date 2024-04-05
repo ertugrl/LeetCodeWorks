3fcac8502958e53432b23b7f8f5164ca3d3f3107
Ertuğrul Şahin <43078415+ertugrl@users.noreply.github.com>
2024-04-05 17:57:29 +0300

    Time: 0 ms (100.00%) | Memory: 8.2 MB (34.23%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -23,3 +23,25 @@ public:
         return result;
     }
 };
+
+
+/* Better solution
+
+class Solution {
+public:
+    string makeGood(string s) 
+    {
+        string res;
+        for (char c : s) {
+            if (!res.empty() && (toupper(c) == toupper(res.back())) && (c - res.back() != 0)) {
+                res.pop_back();
+            } else {
+                res.push_back(c);
+            }
+        }
+        
+        return res;
+    }
+};
+
+*/
