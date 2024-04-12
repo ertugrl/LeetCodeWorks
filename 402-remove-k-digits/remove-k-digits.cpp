fda0fb975463779b385725a898aa6584bcebbbe1
Ertuğrul Şahin <43078415+ertugrl@users.noreply.github.com>
2024-04-12 18:54:21 +0300

    Time: 13 ms (37.13%) | Memory: 10 MB (48.48%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-    // int charToInt(char c){
-    //     return c - '0';
-    // }
     string removeKdigits(string num, int k) {
         if (num.size() == k) return "0";
         
         stack<char> digit;
         digit.push(num[0]);
-
         int cur = 1;
         int removedNumber = 0;
-
         while (cur < num.size() && removedNumber < k){
             if (!digit.empty() && digit.top() > num[cur]){
                 digit.pop();
@@ -21,17 +16,23 @@ public:
                 cur++;
             }
         }
+
+        // If there are still rights to delete digits
         for (int i = 0; i < k - removedNumber; ++i) digit.pop();
 
+        // Combine stack's content and rest of the given string
         string result = "";
         int stackSize = digit.size();
         for (int i = 0; i < stackSize; ++i){
+            // Add digits in reverse order of the desired result
             result += digit.top();
             digit.pop();
         }
         reverse(result.begin(), result.end());
-        result += num.substr(cur);
 
+        result += num.substr(cur); // Rest of the string
+
+        // Remove initial zeros like "0015" -> "15"
         int index;
         for (index = 0; index < result.size(); ++index){
             if (result[index] != '0') return result.substr(index);
