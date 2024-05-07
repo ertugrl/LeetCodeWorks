8e32c448f24bb889584ef8743b482f5319c4e8d4
Ertuğrul Şahin <43078415+ertugrl@users.noreply.github.com>
2024-05-07 17:22:43 +0300

    Time: 151 ms (97.22%) | Memory: 120.7 MB (87.76%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
@@ -11,6 +11,8 @@
 class Solution {
 public:
     ListNode* doubleIt(ListNode* head) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         ListNode* tmp;
         if (head->val >= 5){
             tmp = new ListNode(1);
