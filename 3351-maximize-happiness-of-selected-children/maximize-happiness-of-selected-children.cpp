0f9c20301a5b8c2e2f751126a60344c6b07790a9
Ertuğrul Şahin <43078415+ertugrl@users.noreply.github.com>
2024-05-10 19:26:04 +0300

    Time: 104 ms (98.61%) | Memory: 107.2 MB (59.71%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        map<int, int, greater<int>> mp;
-        for (int i = 0; i < happiness.size(); i++){
-            mp[happiness[i]]++;
-        }
-
-        int selNum = 0;
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        sort(happiness.begin(), happiness.end(), greater<int>());
         long long result = 0;
-        for (auto &it: mp){
-            if (selNum == k || it.first - selNum <= 0) break;
-            for (int i = 0; i < it.second; i++){
-                if (selNum == k || it.first - selNum <= 0) break;
-                result += it.first - selNum++;
-            }
+        for (int i = 0; i < k; i++){
+            if (happiness[i] < i) break;
+            result += happiness[i] - i;
         }
-
         return result;
     }
 };
