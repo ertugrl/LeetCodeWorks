e9b41fc3a70e7ee7235137aa332b5a93b3365675
Ertuğrul Şahin <43078415+ertugrl@users.noreply.github.com>
2024-06-11 13:41:41 +0300

    Time: 9 ms (96.82%) | Memory: 12.1 MB (8.17%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int appendCharacters(string s, string t) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
         int tIndex = 0;
         for (int i = 0; (i < s.size()) && (tIndex < t.size()); ++i){
             if (s[i] == t[tIndex]) tIndex++;
