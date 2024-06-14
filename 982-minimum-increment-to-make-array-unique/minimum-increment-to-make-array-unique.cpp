1e12d7760c09e4668b4d6e738c5191a7198a599e
Ertuğrul Şahin <43078415+ertugrl@users.noreply.github.com>
2024-06-15 00:27:19 +0300

    Time: 904 ms (5.15%) | Memory: 69 MB (79.77%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         sort(nums.begin(), nums.end());
         int result = 0; 
         for (int i = 1; i < nums.size(); ++i){
