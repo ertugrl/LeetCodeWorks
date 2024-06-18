0ddcc496aed575e09a1a0730163072ee9d95d76d
Ertuğrul Şahin <43078415+ertugrl@users.noreply.github.com>
2024-06-18 12:56:47 +0300

    Time: 4 ms (46.50%) | Memory: 7 MB (91.90%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool judgeSquareSum(long long int c) {
-        long long sqr = static_cast<long long>(sqrt(c >> 1));
-        if (sqr * sqr == c >> 1 && !(c & 1)) {
-            return true;
-        }
+        // long long sqr = static_cast<long long>(sqrt(c >> 1));
+        // if (sqr * sqr == c >> 1 && !(c & 1)) {
+        //     return true;
+        // }
 
+        long long sqr = static_cast<long long>(sqrt(c >> 1));
         long long int small = sqr;
-        long long int large = sqr + 1;
+        long long int large = sqr;
         long long int control;
         while (small >= 0) {
             control = pow(small, 2) + pow(large, 2);
