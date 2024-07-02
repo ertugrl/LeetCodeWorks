e3bf6a782efe0934ea6e53b55d4505007871e7c2
Ertuğrul Şahin <43078415+ertugrl@users.noreply.github.com>
2024-07-02 17:24:56 +0300

    Time: 9 ms (11.77%) | Memory: 14.3 MB (15.24%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -8,7 +8,7 @@ public:
         
         vector<int> result;
         for (int i = 0; i < nums2.size(); ++i) {
-            if (mp.find(nums2[i]) != mp.end() && mp[nums2[i]] > 0) {
+            if (mp[nums2[i]] > 0) {
                 result.push_back(nums2[i]);
                 mp[nums2[i]]--;
             }
