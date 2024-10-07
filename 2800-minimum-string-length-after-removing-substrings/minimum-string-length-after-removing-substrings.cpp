6bb58f23d0c08c64dfaaabd4686b376e0d071c69
Ertuğrul Şahin <43078415+ertugrl@users.noreply.github.com>
2024-10-07 13:27:29 +0300

    Time: 4 ms (76.08%) | Memory: 11.6 MB (61.24%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> letters;
+        for (int i = 0; i < s.size(); ++i) {
+            if (!letters.empty() && letters.top() == 'A' && s[i] == 'B') {
+                letters.pop();
+                continue;
+            } else if (!letters.empty() && letters.top() == 'C' && s[i] == 'D') {
+                letters.pop();
+                continue;
+            }
+            letters.push(s[i]);
+        }
+
+        int result = 0;
+        while (!letters.empty()) {
+            letters.pop();
+            result++;
+        }
+
+        return result;
+    }
+};
