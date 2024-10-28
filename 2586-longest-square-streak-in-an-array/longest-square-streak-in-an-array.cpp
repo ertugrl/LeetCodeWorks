3184ea13eabe9f51973e71795d4f1c56f68e9b04
Ertuğrul Şahin <43078415+ertugrl@users.noreply.github.com>
2024-10-28 14:20:43 +0300

    Time: 430 ms (6.67%) | Memory: 108.9 MB (63.34%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -1,3 +1,15 @@
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+#pragma GCC optimize ("O3", "unroll-loops")
+#pragma GCC optimize ("-ffloat-store")
+
+auto init = []() { 
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    return 0;
+} ();
+
+
+
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
